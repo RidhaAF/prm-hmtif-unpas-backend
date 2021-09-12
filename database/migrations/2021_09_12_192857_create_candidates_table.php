41ec21de795aabdb63974473e7c8ad133dd8212b
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-09-13 02:31:25 +0700

    Add Migration candidates

---
+++ database/migrations/2021_09_12_192857_create_candidates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->id();
+            $table->string('nrp')->unique();
+            $table->string('name');
+            $table->string('major')->default('Teknik Informatika');
+            $table->text('vision');
+            $table->text('mission');
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
