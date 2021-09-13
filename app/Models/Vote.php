324874b1564897802e0312bad644a2ef4b6e8ac4
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-09-13 10:47:13 +0700

    Add Model Vote

---
+++ app/Models/Vote.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'candidate_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class, 'candidate_id', 'id');
+    }
+}
