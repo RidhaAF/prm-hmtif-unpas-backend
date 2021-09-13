4d404ae467a6bf0b3d74875b9fb42e119a7124b4
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-09-13 10:47:05 +0700

    Add Model Candidate

---
+++ app/Models/Candidate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Candidate extends Model
+{
+    use HasFactory;
+    use HasProfilePhoto;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'nrp',
+        'name',
+        'major',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function votes()
+    {
+        return $this->hasMany(Vote::class, 'candidate_id', 'id');
+    }
+}
