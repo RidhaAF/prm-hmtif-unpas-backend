2c2ee8fdfebe1e00ceb633fbf104abf42c8ce647
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-11-21 00:01:57 +0700

    Update auth layouts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                     <div class="form-group mb-3">
                         <label for="email" class="form-label text-success"><i class="bi bi-envelope"></i> Email</label>
                         <input type="text" class="form-control @error('email') is-invalid @enderror" name="email"
-                            placeholder="Email" value="{{ old('email') }}" autofocus>
+                            value="{{ old('email') }}" autofocus>
                         @error('email')
                         <div class="invalid-feedback">
                             {{ $message }}
@@ -26,24 +26,15 @@
                         <label for="password" class="form-label text-success"><i class="bi bi-lock"></i> Kata
                             Sandi</label>
                         <input type="password" class="form-control @error('password') is-invalid @enderror"
-                            name="password" placeholder="Kata Sandi">
+                            name="password">
                         @error('password')
                         <div class="invalid-feedback">
                             {{ $message }}
                         </div>
                         @enderror
                     </div>
-                    {{-- <div class="form-check d-flex align-items-end">
-                        <input class="form-check-input me-2" type="checkbox" id="remember_me" name="remember">
-                        <label class="form-check-label text-gray-600" for="remember_me">Ingat saya</label>
-                    </div> --}}
                     <button class="btn btn-success btn-block shadow mt-3" type="submit">Masuk</button>
                 </form>
-                {{-- <div class="text-center mt-5">
-                    <p class="text-gray-600">Belum memiliki akun? <a href="{{ route('register') }}"
-                            class="link-success font-bold">Daftar</a></p>
-                    <p><a class="link-success font-bold" href="{{ route('password.request') }}">Lupa kata sandi?</a></p>
-                </div> --}}
             </div>
         </div>
         <div class="col-lg-7 d-none d-lg-block">
