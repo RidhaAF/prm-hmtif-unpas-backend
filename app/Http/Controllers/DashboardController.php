fd61f8aff6ce760be1500d2aeca52b62f3f4282b
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-04 23:59:26 +0700

    Update query for charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\Candidate;
-use App\Http\Controllers\Controller;
 use App\Models\Vote;
+use App\Models\Candidate;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use ArielMejiaDev\LarapexCharts\LarapexChart;
 
 class DashboardController extends Controller
@@ -17,37 +18,59 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        $chartResult = (new LarapexChart)
-            // ->horizontalBarChart()
-            //     ->addData('Kandidat 1', [Vote::where('candidate_id', '1')->count()])
-            //     ->addData('Kandidat 2', [Vote::where('candidate_id', '2')->count()])
-            //     ->addData('Kandidat 3', [Vote::where('candidate_id', '3')->count()])
-            //     ->addData('Kandidat 4', [Vote::where('candidate_id', '4')->count()])
-            //     ->setXAxis(Candidate::all()->pluck('name')->toArray())
-            //     ->setGrid();
-            ->pieChart()
-            ->addData([Vote::where('candidate_id', '1')->count(), Vote::where('candidate_id', '2')->count(), Vote::where('candidate_id', '3')->count(), Vote::where('candidate_id', '4')->count()])
-            ->setLabels(Candidate::all()->pluck('name')->toArray());
+        $candidates = Candidate::get();
+        $candidateNames = $candidates->pluck('name')->toArray();
+
+        $users = User::join('votes', 'users.id', '=', 'votes.user_id');
+
+        $chartResult = (new LarapexChart)->pieChart()
+            ->addData([
+                Vote::where('candidate_id', 1)->count(),
+                Vote::where('candidate_id', 2)->count(),
+                Vote::where('candidate_id', 3)->count(),
+                Vote::where('candidate_id', 4)->count(),
+            ])
+            ->setLabels($candidateNames);
 
         $chart2019 = (new LarapexChart)->donutChart()
-            ->addData([20, 24, 30, 14])
-            ->setLabels(['Player 7', 'Player 10', 'Player 9', 'Player 8']);
+            ->addData([
+                Vote::where('candidate_id', 1)->count(),
+                Vote::where('candidate_id', 2)->count(),
+                Vote::where('candidate_id', 3)->count(),
+                Vote::where('candidate_id', 4)->count(),
+            ])
+            ->setLabels($candidateNames);
 
         $chart2020 = (new LarapexChart)->donutChart()
-            ->addData([20, 15, 24, 30])
-            ->setLabels(['Player 7', 'Player 10', 'Player 9', 'Player 8']);
+            ->addData([
+                Vote::where('candidate_id', 1)->count(),
+                Vote::where('candidate_id', 2)->count(),
+                Vote::where('candidate_id', 3)->count(),
+                Vote::where('candidate_id', 4)->count(),
+            ])
+            ->setLabels($candidateNames);
 
         $chart2021 = (new LarapexChart)->donutChart()
-            ->addData([20, 24, 7, 30])
-            ->setLabels(['Player 7', 'Player 10', 'Player 9', 'Player 8']);
+            ->addData([
+                Vote::where('candidate_id', 1)->count(),
+                Vote::where('candidate_id', 2)->count(),
+                Vote::where('candidate_id', 3)->count(),
+                Vote::where('candidate_id', 4)->count(),
+            ])
+            ->setLabels($candidateNames);
 
         $chart2022 = (new LarapexChart)->donutChart()
-            ->addData([20, 24, 21, 30])
-            ->setLabels(['Player 7', 'Player 10', 'Player 9', 'Player 8']);
+            ->addData([
+                Vote::where('candidate_id', 1)->count(),
+                Vote::where('candidate_id', 2)->count(),
+                Vote::where('candidate_id', 3)->count(),
+                Vote::where('candidate_id', 4)->count(),
+            ])
+            ->setLabels($candidateNames);
 
         $data = [
             'title' => 'Dashboard',
-            'candidates' => Candidate::get(),
+            'candidates' => $candidates,
             'voters' => User::where('roles', "User")->get(),
             'voted' => User::where('vote_status', 1)->get(),
             'not_voted' => User::where('vote_status', 0)->get(),
