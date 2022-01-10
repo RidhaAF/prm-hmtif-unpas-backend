34b98281ea32bf6a4e616cde59ef3c632a450409
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-11 01:58:06 +0700

    Change year in footer to dynamic year

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted text-end">
-        <p>&copy; 2022 | Made with <span class="text-danger"><i class="bi bi-heart-fill"></i></span> by
+        <p>&copy; {{ date('Y') }} | Made with <span class="text-danger"><i class="bi bi-heart-fill"></i></span> by
             <a href="https://ridhaaf.github.io" target="_blank" class="link-success">Ridha Ahmad Firdaus</a>
         </p>
     </div>
