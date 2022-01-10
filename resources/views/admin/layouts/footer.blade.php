0921846e76fda3ccc5ab44a3df2e128ea8f19996
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2022-01-10 23:43:55 +0700

    Change year in footer to 2022

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,14 +1,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted text-end">
-        <p>&copy; 2021 | Made with <span class="text-danger"><i class="bi bi-heart-fill"></i></span> by
+        <p>&copy; 2022 | Made with <span class="text-danger"><i class="bi bi-heart-fill"></i></span> by
             <a href="https://ridhaaf.github.io" target="_blank" class="link-success">Ridha Ahmad Firdaus</a>
         </p>
-        {{-- <div class="float-start">
-            <p>2021 &copy; Mazer</p>
-        </div>
-        <div class="float-end">
-            <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
-                    href="http://ahmadsaugi.com" class="link-success">A. Saugi</a></p>
-        </div> --}}
     </div>
 </footer>
