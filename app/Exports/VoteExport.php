886768681343ac53546cb24b78f08d3428028006
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2022-02-02 22:27:48 +0700

    Show headings for exported table

--- app/Exports/VoteExport.php
+++ app/Exports/VoteExport.php
@@ -3,15 +3,27 @@
 namespace App\Exports;
 
 use App\Models\Vote;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
-class VoteExport implements FromCollection
+class VoteExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Vote::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'No.',
+            'User ID',
+            'Candidate ID',
+            'Created At',
+            'Updated At',
+        ];
     }
 }
