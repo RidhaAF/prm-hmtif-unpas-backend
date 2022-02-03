a0e95d04110e10ff92abcc0b5dc0d7cea82a1654
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2022-02-03 23:10:22 +0700

    Update error 404 page & asset

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>404 Not Found | PRM HMTIF-UNPAS</title>
+    <title>404 Not Found - PRM HMTIF-UNPAS</title>
 
     {{-- Google Fonts --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -23,23 +23,18 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
-<body style="background-color: #fff;">
-    <div id="app">
-        <div id="main">
-            <section class="min-vh-100">
-                <div class="container">
-                    <div class="row text-center p-5">
-                        <div class="col">
-                            <img src="{{ asset('assets/images/samples/error-404.png') }}" alt="404 Not Found"
-                                class="img-fluid" width="500">
-                            <h3 class="text-success">Oops! Halaman tidak ditemukan :(</h3>
-                            <a class="btn btn-outline-success px-5" href="/">Kembali</a>
-                        </div>
-                    </div>
-                </div>
-            </section>
+<body class="bg-white">
+    <section class="404">
+        <div class="container">
+            <div class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+                <img src="{{ asset('assets/images/404.svg') }}" alt="404 Not Found" class="img-fluid mb-3" width="500">
+                <h1 class="display-1 text-success fw-bold">404 Not Found</h1>
+                <h5 class="text-muted fw-normal mb-3">Oops! Halaman tidak ditemukan..</h4>
+                    <a class="btn btn-outline-success rounded-pill" href="{{ route('admin.dashboard') }}">Kembali ke
+                        Dashboard</a>
+            </div>
         </div>
-    </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
