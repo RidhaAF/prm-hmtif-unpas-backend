7ad85a9c16ea7003c932982760d7517ec73fb7db
ridhaaf <ridhaaf@gmail.com>
2022-04-28 16:30:44 +0700

    Delete unused routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CandidateController;
 use App\Http\Controllers\DashboardController;
@@ -21,19 +20,10 @@ Route::get('/', function () {
     return redirect('login');
 });
 
-// for link storage in production
-Route::get('/linkstorage', function () {
-    Artisan::call('storage:link'); // this will do the command line job
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
     Route::get('admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-    Route::get('admin/votes/export/excel/', [DashboardController::class, 'exportExcel'])->name('admin.votes.export.excel');
-    Route::get('admin/votes/export/pdf/', [DashboardController::class, 'exportPdf'])->name('admin.votes.export.pdf');
+    Route::get('admin/votes/export/excel', [DashboardController::class, 'exportExcel'])->name('admin.votes.export.excel');
+    Route::get('admin/votes/export/pdf', [DashboardController::class, 'exportPdf'])->name('admin.votes.export.pdf');
 
     Route::resource('admin/candidate', CandidateController::class);
     Route::post('admin/candidate/{candidate}', [CandidateController::class, 'deletePhoto'])->name('admin.candidate.delete.photo');
