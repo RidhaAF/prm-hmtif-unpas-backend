b7a0241e43460af7a1529cb7cfdbb505a988e930
ridhaaf <ridhaaf@gmail.com>
2022-06-08 17:35:55 +0700

    Add send password user via email

---
+++ resources/views/emails/send-password.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PRM HMTIF-UNPAS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Hi, {{ $user->name }}!</h1>
+    <p>Ini adalah kata sandi kamu untuk melakukan login pada aplikasi PRM HMTIF-UNPAS</p>
+    <p><b>{{ $user->password }}</b></p>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
