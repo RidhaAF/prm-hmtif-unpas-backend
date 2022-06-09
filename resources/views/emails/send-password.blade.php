8840b3b0eda9186099b9bef87191ab6cd724bc91
ridhaaf <ridhaaf@gmail.com>
2022-06-09 23:47:38 +0700

    Update email message for send password

--- resources/views/emails/send-password.blade.php
+++ resources/views/emails/send-password.blade.php
@@ -11,8 +11,9 @@
 
 <body>
     <h1>Hi, {{ $user->name }}!</h1>
-    <p>Ini adalah kata sandi kamu untuk melakukan login pada aplikasi PRM HMTIF-UNPAS</p>
-    <p><b>{{ $user->password }}</b></p>
+    <p>Ini adalah akun kamu untuk melakukan login pada aplikasi PRM HMTIF-UNPAS</p>
+    <p>NRP: {{ $user->nrp }}</p>
+    <p>Kata Sandi: <b>{{ $user->password }}</b></p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
     </script>
